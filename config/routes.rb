7e33c19011484dc0bf700db0293ee67acc1b668f
Karel van Oordt <vanoordtkarel@gmail.com>
2022-04-15 19:11:38 -0500

    Setup api

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  namespace :api, defaults: { format: 'json' } do
+    get '/messages', to: 'messages#index'
+  end
+
+  get '*page', to: 'static#index', constraints: lambda { |req|
+    !req.xhr? && req.format.html?
+  }
+
+  root 'static#index'
+end
